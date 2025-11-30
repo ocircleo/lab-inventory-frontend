0589ca7b9aaf25d868bea6d1a4b6233019c82fb0
ocircleo <salmanhossain11222626@gmail.com>
2025-11-30 14:31:39 +0600

    last update

--- src/config.js
+++ src/config.js
@@ -2,19 +2,23 @@ const WEB_URLS = [
   "https://lab-inventory-frontend-orpin.vercel.app",
   "http://192.168.0.100:3000",
   "http://192.168.0.120:3000",
+  "http://localhost:3000",
+
 ];
 const API_URLS = [
   "https://lab-inventory-frontend-orpin.vercel.app/api-proxy",
   "http://192.168.0.100:3000/api-proxy",
   "http://192.168.0.120:3000/api-proxy",
+  "http://localhost:3000/api-proxy",
 ];
 const PROXY_URLS = [
   "https://lab-inventory-backend.vercel.app",
   "http://192.168.0.100:5000",
   "http://192.168.0.120:5000",
+  "http://localhost:5000",
 ];
 
-const WEB_URL = WEB_URLS[1];
-const API_URL = API_URLS[1];
-const PROXY_URL = PROXY_URLS[1];
+const WEB_URL = WEB_URLS[3];
+const API_URL = API_URLS[3];
+const PROXY_URL = PROXY_URLS[3];
 export { WEB_URL, API_URL, PROXY_URL };
